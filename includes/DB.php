e08d46e3013e9aeab3b61860f2c7da806bb9aaa6
yaurau <rama7777@bk.ru>
2017-11-15 23:13:06 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	admin/class-plugin-name-admin.php

--- includes/DB.php
+++ includes/DB.php
@@ -18,7 +18,7 @@ class DB
             id INT(11) NOT NULL AUTO_INCREMENT,
             `quote` text,
 			`author` varchar(255),
-            PRIMARY KEY(`id`)); INSERT INTO `wp_yaurau_random_quote`( `quote`, `author`) VALUES              ('Великие начинания даже не надо обдумывать, надо взяться за дело, иначе, заметив                     трудность, отступишь', 'Гай Юлий Цезарь')
+            PRIMARY KEY(`id`)); INSERT INTO `wp_yaurau_random_quote`( `quote`, `author`) VALUES              ('Великие начинания даже не надо обдумывать, надо взяться за дело, иначе, заметив трудность, отступишь', 'Гай Юлий Цезарь')
 EOT;
 
         $this->db->query($sql);
