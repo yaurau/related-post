faaafa8b0c93b8608a68f81c9698a72d2e7a3af1
yaurau <rama7777@bk.ru>
2018-01-27 16:27:16 +0300

    Code optimization

--- public/action.php
+++ public/action.php
@@ -1,22 +1,15 @@
 <?php
-$connect = mysqli_connect('localhost', 'root', '', 'wordpress');
-
+require_once __DIR__ . '/../../../../wp-config.php';
+$connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 $input = filter_input_array(INPUT_POST);
-
-
-$first_name = mysqli_real_escape_string($connect, $input["IP"]);
-$last_name = mysqli_real_escape_string($connect, $input["last_name"]);
-
-
+$IP = mysqli_real_escape_string($connect, $input["IP"]);
 if($input["action"] === 'delete')
 {
     $query = "
  DELETE FROM wp_yaurau_ip_blocked 
- WHERE IP = '".$input["IP"]."'
+ WHERE IP = '".$IP."'
  ";
     mysqli_query($connect, $query);
 }
-
 echo json_encode($input);
-
 ?>
