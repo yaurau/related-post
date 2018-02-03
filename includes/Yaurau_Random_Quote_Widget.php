aee287df5bd8adb7227e3482be3016e4436e773e
yaurau <rama7777@bk.ru>
2018-02-03 10:37:19 +0300

    Create the file uninstall.php

--- includes/Yaurau_Random_Quote_Widget.php
+++ includes/Yaurau_Random_Quote_Widget.php
@@ -3,6 +3,10 @@ require_once __DIR__ . '/../autoload.php';
 
 class Yaurau_Random_Quote_Widget extends WP_Widget
 {
+    /*
+    * Function name: adminWidgetGet
+    * Purpose: include file admin_widget.php
+    */
     public static function adminWidgetGet()
     {
         include_once __DIR__ . '/../public/admin_widget.php';
--- uninstall.php
+++ uninstall.php
@@ -3,3 +3,7 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+$option_name = 'option';
+delete_option($option_name);
+
+
