c99fe78589ad41a6afa500ead1c2731e6165bc9a
yaurau <rama7777@bk.ru>
2018-03-03 15:55:45 +0300

    Code update

--- includes/Yaurau_IP_Blocker.php
+++ includes/Yaurau_IP_Blocker.php
@@ -45,14 +45,14 @@ class Yaurau_IP_Blocker
         if (!empty($_POST)) {
             $signon = wp_signon();
             if (is_wp_error($signon)) {
-                //if($_SERVER['REMOTE_ADDR']!= $_SERVER ['SERVER_ADDR']) {
+                if($_SERVER['REMOTE_ADDR']!= $_SERVER ['SERVER_ADDR']) {
                     if (Yaurau_IP_Blocker_DB::handleIPDB() == NULL) {
                         Yaurau_IP_Blocker_DB::setIPDB();
                     }
                     else {
                         Yaurau_IP_Blocker_Parser::parseQuery();
                     }
-                //}
+                }
             }
         }
     }
