eb5b333dc89ff7070ce45327d53980060e5786b8
yaurau <rama7777@bk.ru>
2018-04-18 20:29:03 +0300

    Debugging code

--- public/add_IPv4.php
+++ public/add_IPv4.php
@@ -1,8 +1,8 @@
 <form action=<?php echo '"options-general.php?page=' . Yaurau_IP_Blocker_PAGE .'"' ?> method="post">
 <label>Enter the IP</label>
-    <input type="text" name="firstblockIP" >/
-    <input type="text" name="secondblockIP" maxlength="2" size="2">
-    <input type="submit" value="Enter" name="submit">
+    <input type="text" name="firstblockIPv4" >/
+    <input type="text" name="secondblockIPv4" maxlength="2" size="2">
+    <input type="submit" value="Enter" name="submitIPv4">
 </form>
 </p>
 <?php
@@ -10,8 +10,8 @@ if(current_user_can(delete_plugins)) {
     $options = [
         'options' => ['min_range' => 0, 'max_range' => 32]
     ];
-    $first = filter_var($_POST['firstblockIP'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
-    $second = filter_var($_POST['secondblockIP'], FILTER_VALIDATE_INT, $options);
+    $first = filter_var($_POST['firstblockIPv4'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+    $second = filter_var($_POST['secondblockIPv4'], FILTER_VALIDATE_INT, $options);
     if (($first !== false && $second !== false)) {
         $l = new Yaurau_IP_Blocker();
         $l->set = $first . '/' . $second;
@@ -22,7 +22,7 @@ if(current_user_can(delete_plugins)) {
         $l->set = $first;
         $l->enterIP();
         echo "IP $l->set blocked";
-    } elseif ($_POST['submit']) {
+    } elseif ($_POST['submitIPv4']) {
         echo "Incorrect IP";
     }
 }
